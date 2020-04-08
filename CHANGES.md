cd798c212195815e87e5ef552832029b384a05cf
Brett Zamir <brettz9@yahoo.com>
2020-04-08 08:42:11 +0800

    - Update per latest `rollup-plugin-filesize` fork API - npm: Update devDeps - npm: Bump to 0.2.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,10 @@
 # CHANGES for `filesize-badger`
 
+## 0.2.0
+
+- Update per latest `rollup-plugin-filesize` fork API
+- npm: Update devDeps
+
 ## 0.1.0
 
 - Initial version
--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ function getRollupObject ({
   return {
     plugins: [
       filesize({
+        showBeforeSizes: true,
         postRender: filesizeBadger()
       })
     ],
